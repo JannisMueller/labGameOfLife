a69ab36355b622723268202d681caa19753f3fe3
JannisMueller <jannis.mueller@hotmail.de>
2021-10-28 09:17:30 +0200

    refaccdirst test for initializing grid

---
+++ src/main/java/se/mueller/UserInput.java
@@ -0,0 +1,15 @@
+package se.mueller
+
+class UserInput {
+
+
+
+    int indexOfColumn;
+    int indexOfRow;
+
+    UserInput(int indexOfColumn, int indexOfRow) {
+        this.indexOfColumn = indexOfColumn
+        this.indexOfRow = indexOfRow
+    }
+
+}
